730df1b4994fbeed89f710285dd9cd2649527274
reza aditya <rezaadityaa26@gmail.com>
2026-05-05 18:56:40 +0700

    forgot to add reviewroute.js

--- src/routes/reviewRoute.js
+++ src/routes/reviewRoute.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { createReview, getReviews } from "../controllers/reviewController.js";
+import { createReview, getReviews, updateReview, deleteReview } from "../controllers/reviewController.js";
 import { jwtMiddleware } from "../middlewares/authMiddleware.js";
 
 const routeReview = express.Router();
 
 // route protected
 routeReview.post('/create', jwtMiddleware, createReview);
+routeReview.put("/:id", jwtMiddleware, updateReview);
+routeReview.delete("/:id", jwtMiddleware, deleteReview);
 
 // route public
 routeReview.get('/:id', getReviews);
