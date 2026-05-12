6cc8e2ce53807761e54df1371aeac099c946def1
galihpikun <galihrakha003@gmail.com>
2026-05-12 08:01:55 +0700

    feat:get hot resto

--- src/controllers/restaurantController.js
+++ src/controllers/restaurantController.js
@@ -1,92 +1,138 @@
-    import { prisma } from "../config/db.js";
+import { prisma } from "../config/db.js";
 
-    export const createRestaurant = async (req, res) => {
-        const {name, address, description} = req.body;
-        const userId = req.user.id;
-        const userRole = req.user.role;
+export const createRestaurant = async (req, res) => {
+  const { name, address, description } = req.body;
+  const userId = req.user.id;
+  const userRole = req.user.role;
 
-        // validasi role (role merchant doang yang boleh create resto)
-        if (userRole !== 'MERCHANT') {
-            return res.status(403).json({
-                success: false,
-                message: "role tidak sesuai, tidak boleh membuat resto"
-            })
-        }
+  // validasi role (role merchant doang yang boleh create resto)
+  if (userRole !== "MERCHANT") {
+    return res.status(403).json({
+      success: false,
+      message: "role tidak sesuai, tidak boleh membuat resto",
+    });
+  }
 
-        // validasi field
-        if (!name || !address) {
-            return res.status(400).json({
-                success: false,
-                message: "belom lengkap"
-            })
-        }
+  // validasi field
+  if (!name || !address) {
+    return res.status(400).json({
+      success: false,
+      message: "belom lengkap",
+    });
+  }
 
-        try {
-            const existingResto = await prisma.restaurant.findUnique({
-                where: {
-                    userId: userId
-                }
-            });
+  try {
+    const existingResto = await prisma.restaurant.findUnique({
+      where: {
+        userId: userId,
+      },
+    });
 
-            if (existingResto) {
-                return res.status(400).json({
-                    success: false,
-                    message: "restoran udah ada"
-                });
-            }
-
-            const restaurant = await prisma.restaurant.create({
-                data: {
-                    name: name,
-                    address: address,
-                    description: description || null,
-                    userId: userId
-                }
-            });
-
-            return res.status(200).json({
-                success: true,
-                message: "Restoran berhasil dibuat",
-                data: restaurant
-            });
-
-        } catch (error) {
-            console.error(error);
-            return res.status(500).json({
-                success: false,
-                message: "Internal Server Error"
-            })
-        }
+    if (existingResto) {
+      return res.status(400).json({
+        success: false,
+        message: "restoran udah ada",
+      });
     }
 
-    export const getRestaurantById = async (req, res) => {
-        const { id } = req.params;
+    const restaurant = await prisma.restaurant.create({
+      data: {
+        name: name,
+        address: address,
+        description: description || null,
+        userId: userId,
+      },
+    });
 
-        try {
-            const restaurant = await prisma.restaurant.findUnique({
-                where: { id: id},
-                include: {
-                    products: true, // return semua produk yg ada di resto ini
-                }, 
-            });
+    return res.status(200).json({
+      success: true,
+      message: "Restoran berhasil dibuat",
+      data: restaurant,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
 
-            if (!restaurant) {
-                return res.status(404).json({
-                    success: false,
-                    message: "resto ga ada"
-                })
-            }
+export const getRestaurantById = async (req, res) => {
+  const { id } = req.params;
 
-            return res.status(200).json({
-                success: true,
-                message: "resto ditemukan",
-                data: restaurant
-            });
-        } catch (error) {
-            console.error(error);
-            return res.status(500).json({
-                success: false,
-                message: "Internal Server Error"
-            })
-        }
+  try {
+    const restaurant = await prisma.restaurant.findUnique({
+      where: { id: id },
+      include: {
+        products: true, // return semua produk yg ada di resto ini
+      },
+    });
+
+    if (!restaurant) {
+      return res.status(404).json({
+        success: false,
+        message: "resto ga ada",
+      });
     }
+
+    return res.status(200).json({
+      success: true,
+      message: "resto ditemukan",
+      data: restaurant,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export const getHotRestaurants = async (req, res) => {
+  try {
+    const restaurants = await prisma.restaurant.findMany({
+      include: {
+        products: {
+          include: {
+            _count: {
+              select: {
+                orders: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    // hitung total order tiap resto
+    const hotRestaurants = restaurants.map((restaurant) => {
+      const totalOrders = restaurant.products.reduce(
+        (total, product) => total + product._count.orders,
+        0
+      );
+
+      return {
+        ...restaurant,
+        totalOrders,
+      };
+    });
+
+    // sort paling banyak odernya
+    hotRestaurants.sort((a, b) => b.totalOrders - a.totalOrders);
+
+    return res.status(200).json({
+      success: true,
+      message: "Hot restaurants retrieved successfully",
+      data: hotRestaurants,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
--- src/routes/restaurantRoute.js
+++ src/routes/restaurantRoute.js
@@ -1,13 +1,13 @@
 import express from "express";
-import { createRestaurant, getRestaurantById } from "../controllers/restaurantController.js";
+import { createRestaurant, getHotRestaurants, getRestaurantById } from "../controllers/restaurantController.js";
 import { jwtMiddleware } from "../middlewares/authMiddleware.js";
 
 const routeRestaurant = express.Router();
 
 // route statis
 routeRestaurant.post('/', jwtMiddleware, createRestaurant);
-
+routeRestaurant.get("/hot", jwtMiddleware,getHotRestaurants);
 // route dinamis
-routeRestaurant.get('/:id', getRestaurantById);
+routeRestaurant.get('/:id', jwtMiddleware, getRestaurantById);
 
 export default routeRestaurant;
